abad73f38fe914f82b329845e907d333411cf68a
tajul51ict <166229717+tajul51ict@users.noreply.github.com>
2026-02-16 17:07:13 +0600

    Create Department.java

---
+++ LAB FINAL LIVE/Ques1/src/main/java/Department.java
@@ -0,0 +1,15 @@
+public class Department {
+
+    private int id;
+    private String deptName;
+    private int totalStudent;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getDeptName() { return deptName; }
+    public void setDeptName(String deptName) { this.deptName = deptName; }
+
+    public int getTotalStudent() { return totalStudent; }
+    public void setTotalStudent(int totalStudent) { this.totalStudent = totalStudent; }
+}
