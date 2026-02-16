606f83305dcc4cc34e17ba3ccb444ad85c249f2a
tajul51ict <166229717+tajul51ict@users.noreply.github.com>
2026-02-16 17:31:56 +0600

    Create TestSpringJpaApplicationTests.java

---
+++ LAB FINAL LIVE/Ques2/src/test/java/example/TestSpringJPA/TestSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestSpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
