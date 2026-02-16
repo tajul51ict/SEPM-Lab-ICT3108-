29bf6c542a84e7e68e2c232e7defdb6fa62abfdf
tajul51ict <166229717+tajul51ict@users.noreply.github.com>
2026-02-16 17:21:27 +0600

    Create TestSpringJpaApplication.java

---
+++ LAB FINAL LIVE/Ques2/src/main/java/TestSpringJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.TestSpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringJpaApplication.class, args);
+	}
+}
